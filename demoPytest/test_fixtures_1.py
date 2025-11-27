5d988ee20ddde80fb963ace063c03b6f97b6ef73
mohamedthowfiq <mohamedthowfiq9626@gmail.com>
2025-11-27 17:51:26 +0530

    test_fixtures_2,scopes,fixtures using markers

--- demoPytest/test_fixtures_1.py
+++ demoPytest/test_fixtures_1.py
@@ -5,19 +5,33 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.maximize_window()
 
-@pytest.fixture()
+@pytest.fixture(autouse=True)
+def start_automatic_ficture():
+    print("Start Test with automatic fixture")
+
 #another name for post condition is teardown
+@pytest.fixture(scope="function")
 def setup_teardown():
     driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-    time.sleep(3)
+    time.sleep(4)
     driver.find_element(By.XPATH,"//input[@name='username']").send_keys("Admin")
     driver.find_element(By.XPATH,"//input[@name='password']").send_keys("admin123")
     driver.find_element(By.XPATH,"//button[text()=' Login ']").click()
+    print("Login in")
     yield
+    time.sleep(4)
     driver.find_element(By.XPATH,"//p[@class='oxd-userdropdown-name']").click()
     driver.find_element(By.XPATH,"//a[text()='Logout']").click()
+    print("Log out")
 
-    def test1_Admin_title(setup_teardown):
-        driver.find_element(By.XPATH,"//span[text()='Admin']").click()
-        assert driver.title == "OrangeHRM"
-        print("Test 1 is complete")
+def test1_Admin_title(setup_teardown):
+    time.sleep(4)
+    driver.find_element(By.XPATH,"//span[text()='Admin']").click()
+    assert driver.title == "OrangeHRM"
+    print("Test 1 is complete")
+
+def test2_PIM_title(setup_teardown):
+    time.sleep(4)
+    driver.find_element(By.XPATH,"//span[text()='PIM']").click()
+    assert driver.title == "OrangeHRM"
+    print("Test 2 is complete")
---
+++ demoPytest/test_fixtures_2.py
@@ -0,0 +1,40 @@
+import time,pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+@pytest.fixture(autouse=True)
+def start_automatic_ficture():
+    print("Start Test with automatic fixture")
+
+#another name for post condition is teardown
+@pytest.fixture(scope="function")
+def setup_teardown():
+    driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    time.sleep(4)
+    driver.find_element(By.XPATH,"//input[@name='username']").send_keys("Admin")
+    driver.find_element(By.XPATH,"//input[@name='password']").send_keys("admin123")
+    driver.find_element(By.XPATH,"//button[text()=' Login ']").click()
+    print("Login in")
+    yield
+    time.sleep(4)
+    driver.find_element(By.XPATH,"//p[@class='oxd-userdropdown-name']").click()
+    driver.find_element(By.XPATH,"//a[text()='Logout']").click()
+    print("Log out")
+
+
+@pytest.mark.usefixtures("setup_teardown")# using mark for fixture
+def test1_Admin_title():
+    time.sleep(4)
+    driver.find_element(By.XPATH,"//span[text()='Admin']").click()
+    assert driver.title == "OrangeHRM"
+    print("Test 1 is complete")
+
+@pytest.mark.usefixtures("setup_teardown")# using mark for fixture
+def test2_PIM_title():
+    time.sleep(4)
+    driver.find_element(By.XPATH,"//span[text()='PIM']").click()
+    assert driver.title == "OrangeHRM"
+    print("Test 2 is complete")
